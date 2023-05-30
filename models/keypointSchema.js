d0a19259de695f4c4695b3ab1c81c04c7329f6c9
ChangKi <changkiheon@naver.com>
2023-05-30 11:13:18 +0900

    update keypoint schema

--- models/keypointSchema.js
+++ models/keypointSchema.js
@@ -4,7 +4,6 @@ const mongoose = require('mongoose');
 
 
 const keypointSchema = new mongoose.Schema({
-  kypt_id: { type: String, unique: true },
   kypt_name: {
     type: String,
     required: true
