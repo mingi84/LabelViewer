4b74ea4221cc8a328090ee46e7b4dc13eb6354c8
misun <misun@gazzi.ai>
2023-06-13 14:40:32 +0900

    imageviewer image url global const

--- public/js/globalconst.js
+++ public/js/globalconst.js
@@ -2,3 +2,6 @@ const _WEBSERVER_ROOT_URL = "http://49.50.160.122:9070/OpenAPI/";
 const _WEBSERVER_DI_URL = "http://49.50.160.122:80/";
 
 const _PAGE_URL = "http://127.0.0.1:8008/"
+
+//ORIGINAL IMAGE, ORIGINAL LABEL FILE LOCATION
+const _ORIGIN_LOCATION = "D:\\MgnSData\\Datas\\";
